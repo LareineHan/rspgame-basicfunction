d64064b5ef1712621b892a745a56b50f5d58708f
Lareine Han <lareine.han@gmail.com>
2024-03-17 20:48:37 -0700

    first commit for hw 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Box from './component/Box';
+import rock from './img/rock.png';
+import scissors from './img/scissors.png';
+import paper from './img/paper.png';
+import rockbtn from './img/rock.png';
+import scissorsbtn from './img/scissors.png';
+import paperbtn from './img/paper.png';
+
+const choice = {
+	rock: {
+		name: 'Rock',
+		img: rock,
+	},
+	scissors: {
+		name: 'Scissors',
+		img: scissors,
+	},
+	paper: {
+		name: 'Paper',
+		img: paper,
+	},
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [userSelect, setUserSelect] = useState(null);
+	const [computerSelect, setComputerSelect] = useState(null);
+	const [result, setResult] = useState('');
+
+	const play = (userChoice) => {
+		setUserSelect(choice[userChoice]);
+		let computerChoice = randomChoice();
+		setComputerSelect(computerChoice);
+		judge(choice[userChoice], computerChoice);
+		setResult(judge(choice[userChoice], computerChoice));
+	};
+
+	const judge = (user, computer) => {
+		if (user.name === computer.name) {
+			return 'Draw';
+		} else if (
+			(user.name === 'Rock' && computer.name === 'Scissors') ||
+			(user.name === 'Scissors' && computer.name === 'Paper') ||
+			(user.name === 'Paper' && computer.name === 'Rock')
+		) {
+			return 'Win';
+		} else {
+			return 'Lose';
+		}
+	};
+
+	const randomChoice = () => {
+		let itemArray = Object.keys(choice);
+		let randomIndex = Math.floor(Math.random() * itemArray.length);
+		let computerChoice = itemArray[randomIndex];
+		console.log('computer choice', computerChoice);
+		return choice[computerChoice];
+	};
+
+	return (
+		<div className='App'>
+			<Container>
+				<div className='App-header'>
+					<h2>Rock Paper Scissors</h2>
+				</div>
+
+				<div className='App-middle'>
+					<div className='box-container' id='screen'>
+						<Box title='You' item={userSelect} result={result} />
+						<Box title='Computer' item={computerSelect} result={result} />
+					</div>
+				</div>
+				<div className='App-bottom'>
+					<div className='btn-container'>
+						<button className='rsp-btn' onClick={() => play('rock')}>
+							<img src={rockbtn} alt='rock' />
+						</button>
+						<button className='rsp-btn' onClick={() => play('scissors')}>
+							{' '}
+							<img src={scissorsbtn} alt='rock' />
+						</button>
+						<button className='rsp-btn' onClick={() => play('paper')}>
+							{' '}
+							<img src={paperbtn} alt='rock' />
+						</button>
+					</div>
+				</div>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
